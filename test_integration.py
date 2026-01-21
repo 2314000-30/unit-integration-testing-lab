6eaff4bc77586208247560d714bd0c23fbcb8393
Asma Hussain <asma@example.com>
2026-01-21 20:58:01 +0500

    Add banking app with unit and integration tests

--- test_integration.py
+++ test_integration.py
@@ -15,7 +15,7 @@ def test_transfer_insufficient_balance():
 def test_transfer_then_interest():
     b_from, b_to = transfer(10000, 5000, 2000)
     updated_balance = calculate_interest(b_to, 10, 1)
-    assert updated_balance == 7700.0
+    assert updated_balance == pytest.approx(7700.0)
 
 
 def test_transfer_invalid_amount():
